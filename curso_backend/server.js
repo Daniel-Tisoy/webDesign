5de5fa244c788777449d2d4238f8fdda2c4761a9
ltisoy <danieltisoy2001@gmail.com>
2021-11-24 15:14:00 -0500

    recibiendo datos de la peticion via query params

--- curso_backend/server.js
+++ curso_backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World express");
+// express genera un objeto que viene de la peticion,
+// y se recibe el primer argumento de la funcion manejadora
+// en este caso recibimos los datos via parametros de consulta (query params)
+// ejemplo: http://localhost:3000/users?name=juan
+app.get("/saludo", (req, res) => {
+  res.send(`hola ${req.query.name}`);
 });
 
 app.listen(3000);
